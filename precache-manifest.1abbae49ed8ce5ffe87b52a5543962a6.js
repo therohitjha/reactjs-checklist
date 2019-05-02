a2e2cedd123838b25b2d94971ace1a28a47b9382
Rohit Jha <45750703+therohitjha@users.noreply.github.com>
2019-05-02 11:52:35 +0530

    Updates

---
+++ precache-manifest.1abbae49ed8ce5ffe87b52a5543962a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcaf93da37e07672ad470995aebffff",
+    "url": "/reactjs-checklist/index.html"
+  },
+  {
+    "revision": "afb337556b8eec06864b",
+    "url": "/reactjs-checklist/static/css/main.3a197b89.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/reactjs-checklist/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "afb337556b8eec06864b",
+    "url": "/reactjs-checklist/static/js/main.f473c232.chunk.js"
+  },
+  {
+    "revision": "acd1dd3f39825c54b218",
+    "url": "/reactjs-checklist/static/js/runtime~main.bb1914c6.js"
+  }
+]);
